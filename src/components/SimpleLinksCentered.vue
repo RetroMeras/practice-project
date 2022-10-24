fbc2c627b5a3d9eeaa71ef21c9fae3aa12c36fd4
ardoriaa <dasha.urvantseva@gmail.com>
2022-10-24 14:52:23 +0300

    pricing

--- src/components/SimpleLinksCentered.vue
+++ src/components/SimpleLinksCentered.vue
@@ -108,7 +108,7 @@
         </Popover>
 
         <a
-          href="#"
+          href="/pricing"
           class="text-base font-medium text-gray-500 hover:text-gray-900"
         >
           Pricing
@@ -204,61 +204,12 @@ const solutions = [
     href: "#",
     icon: ChartBarIcon,
   },
-  {
-    name: "Engagement",
-    description: "Speak directly to your customers in a more meaningful way.",
-    href: "#",
-    icon: CursorClickIcon,
-  },
-  {
-    name: "Security",
-    description: "Your customers' data will be safe and secure.",
-    href: "#",
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools that you're already using.",
-    href: "#",
-    icon: ViewGridIcon,
-  },
-  {
-    name: "Automations",
-    description:
-      "Build strategic funnels that will drive your customers to convert",
-    href: "#",
-    icon: RefreshIcon,
-  },
-  {
-    name: "Reports",
-    description:
-      "Get detailed reports that will help you make more informed decisions ",
-    href: "#",
-    icon: DocumentReportIcon,
-  },
 ];
 const resources = [
   {
     name: "Help Center",
     description:
       "Get all of your questions answered in our forums or contact support.",
-    href: "#",
-  },
-  {
-    name: "Guides",
-    description:
-      "Learn how to maximize our platform to get the most out of it.",
-    href: "#",
-  },
-  {
-    name: "Events",
-    description:
-      "See what meet-ups and other events we might be planning near you.",
-    href: "#",
-  },
-  {
-    name: "Security",
-    description: "Understand how we take your privacy seriously.",
     href: "#",
   },
 ];
