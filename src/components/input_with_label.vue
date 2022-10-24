ef9e494ea891d521bb1bc101efd01305245c296f
melaveetha <retromeras@ya.ru>
2022-10-24 15:18:48 +0300

    Add [input]

---
+++ src/components/input_with_label.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  value: string;
+  placeholder: string;
+}>();
+const emit = defineEmits(["update:value"]);
+</script>
+<template>
+  <div>
+    <label for="email" class="block text-sm font-medium text-gray-700">{{
+      props.label
+    }}</label>
+    <div class="mt-1">
+      <input
+        :value="props.value"
+        :placeholder="props.placeholder"
+        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+        @input="emit('update:value', $event.target?.value || '')"
+      />
+    </div>
+  </div>
+</template>
--- src/routes/Main.vue
+++ src/routes/Main.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import Input_with_label from "../components/input_with_label.vue";
+
+const value = ref("");
+</script>
 
 <template>
-  <div>123123</div>
+  <div>Value: {{ value }}</div>
+  <Input_with_label v-model:value="value" label="Value" placeholder="" />
 </template>
