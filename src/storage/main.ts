dbdc142f3cb5f9962365ec9ce96174ee04ffbdf1
melaveetha <retromeras@ya.ru>
2022-11-05 16:28:21 +0300

    Add [save to localstorage]

--- src/storage/main.ts
+++ src/storage/main.ts
@@ -3,11 +3,36 @@ import { computed, ref } from 'vue'
 import { IEntity } from '../types/IEntity'
 import { IRelation } from '../types/IRelation'
 
-// https://pinia.vuejs.org/core-concepts/#option-stores
 
+const updateEntites = (entities: IEntity[]) => {
+  const storage = window.localStorage;
+  storage.setItem("entities", JSON.stringify(entities))
+}
+const updateRelations = (relations: IRelation[]) => {
+  const storage = window.localStorage;
+  storage.setItem("relations", JSON.stringify(relations))
+}
+
+
+const getEntities = () => {
+  const storage = window.localStorage;
+  const entities = storage.getItem('entities')
+  if (!entities) return null
+  return JSON.parse(entities) as IEntity[]
+}
+const getRelations = () => {
+  const storage = window.localStorage;
+  const relations = storage.getItem('relations')
+  if (!relations) return null
+  return JSON.parse(relations) as IRelation[]
+}
+
+
+
+// https://pinia.vuejs.org/core-concepts/#option-stores
 export const useMainStore = defineStore('main', () =>{
-  const entities = ref<IEntity[]>([]);
-  const relations = ref<IRelation[]>([]);
+  const entities = ref<IEntity[]>(getEntities() || []);
+  const relations = ref<IRelation[]>(getRelations() || []);
 
   const emptyEntity = computed(() => ({
     id: '',
@@ -27,6 +52,7 @@ export const useMainStore = defineStore('main', () =>{
   } as IRelation))
   const addEntity = (entity: IEntity) => {
     entities.value.push(entity);
+    updateEntites(entities.value);
   }
   const addRelation = (relation: IRelation): boolean => {
     const firstIdx = entities.value.findIndex((entity) => entity.id == relation.parents[0]);
@@ -36,6 +62,9 @@ export const useMainStore = defineStore('main', () =>{
     relations.value.push(relation);
     entities.value[firstIdx].relations.push(relation.id)
     entities.value[secondIdx].relations.push(relation.id)
+
+    updateRelations(relations.value)
+    updateEntites(entities.value)
 
     return true
   }
