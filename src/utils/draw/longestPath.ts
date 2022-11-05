74182c80e4d59b2ad6df665be0f77f8664de2784
melaveetha <retromeras@ya.ru>
2022-11-05 16:56:25 +0300

    Draft [draw todo]

---
+++ src/utils/draw/longestPath.ts
@@ -0,0 +1,5 @@
+export const longestPath = (graph: any[][][]) => {
+  // Поиск длиннейшего пути в графе (https://ru.wikipedia.org/wiki/%D0%9F%D1%83%D1%82%D1%8C_(%D1%82%D0%B5%D0%BE%D1%80%D0%B8%D1%8F_%D0%B3%D1%80%D0%B0%D1%84%D0%BE%D0%B2))
+  // гугли какой нибудь алгоритм
+  return true
+}
