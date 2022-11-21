1c786819a879d8bb4db649e0d8689ea4f13ea74f
melaveetha <retromeras@ya.ru>
2022-11-21 15:21:14 +0300

    Draft [new list element] list

--- src/components/basic/List.vue
+++ src/components/basic/List.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 defineProps<{
   items: any[];
+  titles: string[];
 }>();
+
+const selected = ref<number | undefined>(undefined);
+
+const handleSelect = (index: number) => {
+  selected.value = index;
+};
 </script>
 
 <template>
   <div>
-    <slot v-for="item in items" :item="item"></slot>
+    <table>
+      <tr>
+        <th
+          v-for="(title, index) in titles"
+          :key="index"
+          class="sticky top-0 z-1 bg-pink-300"
+        >
+          {{ title }}
+        </th>
+      </tr>
+      <tr>
+        <slot
+          v-for="(item, index) in items"
+          :item="item"
+          :select="handleSelect(index)"
+        ></slot>
+      </tr>
+    </table>
   </div>
 </template>
