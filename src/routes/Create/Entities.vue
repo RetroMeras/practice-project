8f0d6fc42af4974abfdd2543e29151ddc8b77d28
ardoriaa <dasha.urvantseva@gmail.com>
2022-11-21 16:46:50 +0300

    фвв дшые

--- src/routes/Create/Entities.vue
+++ src/routes/Create/Entities.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
+import { PlusIcon } from "vue-tabler-icons";
 import List from "../../components/basic/List.vue";
+import EntityAdd from "../../components/Entity/EntityAdd.vue";
+import EntityDelete from "../../components/Entity/EntityDelete.vue";
+import EntityEdit from "../../components/Entity/EntityEdit.vue";
 import EntityItem from "../../components/Entity/EntityItem.vue";
 import { useMainStore } from "../../storage/main";
 
@@ -9,12 +13,29 @@ const { entities } = storeToRefs(store);
 </script>
 
 <template>
-  <List v-slot="slotProps" :items="entities" :titles="['Название', 'Описание']">
-    <EntityItem
-      :item="slotProps.item"
-      :select="slotProps.select"
-      :odd="slotProps.index % 2 !== 0"
-      :selected="slotProps.selected"
-    />
+  <List :items="entities" :titles="['Название', 'Описание']">
+    <template #utils="utilsProps">
+      <div class="flex flex-row w-min gap-2 my-3">
+        <EntityAdd><PlusIcon color="green" /></EntityAdd>
+        <EntityEdit
+          v-if="utilsProps.selected"
+          :entity="entities[utilsProps.selected]"
+          :after="utilsProps.clear"
+        />
+        <EntityDelete
+          v-if="utilsProps.selected"
+          :uuid="entities[utilsProps.selected].uuid"
+          :after="utilsProps.clear"
+        />
+      </div>
+    </template>
+    <template #item="itemProps">
+      <EntityItem
+        :item="itemProps.item"
+        :select="itemProps.select"
+        :odd="itemProps.index % 2 !== 0"
+        :selected="itemProps.selected"
+      />
+    </template>
   </List>
 </template>
