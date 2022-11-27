ae56cac54ba75cef1a01fd109bc262000e9e73f9
melaveetha <retromeras@ya.ru>
2022-11-27 13:29:16 +0300

    Draft [d3js forceGraph] draw

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './routes/router'
 import { createPinia } from 'pinia'
-import VueKonva from 'vue-konva';
-
 
 const pinia = createPinia();
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
-app.use(VueKonva)
 
 app.mount('#app')
--- src/routes/Draw.vue
+++ src/routes/Draw.vue
@@ -1,67 +1,93 @@
 <script setup lang="ts">
-import Circle, { ICircle } from "../components/Draw/Circle.vue";
-import Relation from "../components/Draw/Relation.vue";
+import * as d3 from "d3";
+import { onMounted } from "vue";
 
-const konvaConfigs = {
-  width: 500,
-  height: 500,
-};
-
-const circles: ICircle[] = [
-  {
-    x: 100,
-    y: 100,
-    radius: 50,
-    label: "0",
-  },
-  {
-    x: 100,
-    y: 300,
-    radius: 50,
-    label: "1",
-  },
-  {
-    x: 300,
-    y: 300,
-    radius: 30,
-    label: "2",
-  },
-  {
-    x: 300,
-    y: 200,
-    radius: 25,
-    label: "3",
-  },
+const data = [
+  { id: "1", value: 10, label: "носки" },
+  { id: "2", value: 20, label: "шорты" },
+  { id: "3", value: 40, label: "штаны" },
+  { id: "4", value: 90, label: "куртка" },
+];
+const links = [
+  { id: "1", target: "1", source: "2" },
+  { id: "2", target: "2", source: "1" },
+  { id: "3", target: "1", source: "3" },
+  { id: "4", target: "3", source: "4" },
 ];
 
-const relations = [
-  [0, 1],
-  [0, 2],
-  [2, 1],
-  [0, 3],
-  [3, 1],
-];
+const WIDTH = 500;
+const HEIGHT = 500;
+const LINK_DISTANCE = 200;
+const RADIUS = 20;
+
+onMounted(() => {
+  const simulation = d3
+    .forceSimulation()
+    .force("charge", d3.forceManyBody())
+    .force(
+      "link",
+      d3
+        .forceLink()
+        .distance(LINK_DISTANCE)
+        .id((d) => d.id)
+    )
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .on("tick", ticked);
+  const container = d3
+    .select("#canvas")
+    .classed("canvas", true)
+    .attr("viewBox", [-WIDTH / 2, -HEIGHT / 2, WIDTH, HEIGHT]);
+
+  const link = container
+    .append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-width", 1.5)
+    .selectAll("line")
+    .data(links, (link) => [link.source, link.target])
+    .join("line");
+
+  const node = container
+    .append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(data, (node) => node.id)
+    .join((enter) => enter.append("circle"))
+    .attr("r", RADIUS)
+    .attr("fill", "red");
+  // .text((node) => node.label);
+  // .call((node) => node.append("title").text((node) => node.label));
+
+  function ticked() {
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
+  }
+  simulation.nodes(data);
+  simulation.force("link").links(links);
+  simulation.alpha(1).restart().tick();
+  ticked();
+});
 </script>
 
 <template>
   <div>
-    <v-stage :config="konvaConfigs">
-      <v-layer>
-        <Relation
-          v-for="(relation, idx) in relations"
-          :key="idx"
-          :first="circles[relation[0]]"
-          :second="circles[relation[1]]"
-        />
-        <Circle
-          v-for="(circle, idx) in circles"
-          :key="idx"
-          :x="circle.x"
-          :y="circle.y"
-          :radius="circle.radius"
-          :label="circle.label"
-        />
-      </v-layer>
-    </v-stage>
+    <svg id="canvas"></svg>
   </div>
 </template>
+
+<style>
+.canvas {
+  width: 500px;
+  height: 500px;
+  border: 1px solid red;
+}
+
+.bar {
+  fill: #3f4f5f;
+}
+</style>
