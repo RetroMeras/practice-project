5ada1f39eee23cd805f804512cd754009ef00d74
melaveetha <retromeras@ya.ru>
2022-12-11 16:12:50 +0300

    Add [Tree] diagram

---
+++ src/components/Draw/Tree.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { Tree } from "../../utils/draw/tree";
+
+onMounted(() => {
+  const chart = Tree(
+    "#treeContainer",
+    {
+      material: "Человек",
+      company: {
+        name: "Я",
+        url: "https://www.google.com",
+      },
+      children: [
+        {
+          material: "Часть мамы",
+          company: {
+            name: "Мама",
+            url: "https://www.google.com",
+          },
+          children: [
+            {
+              material: "Часть мамы мамы",
+              company: {
+                name: "Мама мамы",
+                url: "https://www.google.com",
+              },
+              children: [
+                {
+                  material: "Часть мамы мамы мамы",
+                  company: {
+                    name: "Мама мамы мамы",
+                    url: "https://www.google.com",
+                  },
+                  children: [],
+                },
+                {
+                  material: "Часть папы мамы мамы",
+                  company: {
+                    name: "Папа мамы мамы",
+                    url: "https://www.google.com",
+                  },
+                  children: [],
+                },
+              ],
+            },
+            {
+              material: "Часть папы мамы",
+              company: {
+                name: "Папа Мамы",
+                url: "https://www.google.com",
+              },
+              children: [],
+            },
+          ],
+        },
+        {
+          material: "Часть папы",
+          company: {
+            name: "Папа",
+            url: "https://www.google.com",
+          },
+          children: [
+            {
+              material: "Часть мамы папы",
+              company: {
+                name: "Мама папы",
+                url: "https://www.google.com",
+              },
+              children: [],
+            },
+            {
+              material: "Часть папы папы",
+              company: {
+                name: "Папа папы",
+                url: "https://www.google.com",
+              },
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      dx: 30,
+    }
+  );
+});
+</script>
+
+<template>
+  <svg id="treeContainer"></svg>
+</template>
--- src/routes/Draw.vue
+++ src/routes/Draw.vue
@@ -1,48 +1,9 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { forceGraph, INode, ILink } from "../utils/draw/forceGraph";
-
-const nodes: INode[] = [
-  { id: "1", label: "1" },
-  { id: "2", label: "2" },
-  { id: "3", label: "3" },
-  { id: "4", label: "4" },
-  { id: "5", label: "5" },
-  { id: "6", label: "6" },
-  { id: "7", label: "7" },
-  { id: "8", label: "8" },
-];
-const links: ILink[] = [
-  { id: "1", target: "1", source: "2" },
-  { id: "2", target: "1", source: "3" },
-  { id: "3", target: "1", source: "4" },
-  { id: "4", target: "1", source: "5" },
-  { id: "5", target: "1", source: "7" },
-  { id: "6", target: "2", source: "7" },
-  { id: "7", target: "3", source: "5" },
-  { id: "8", target: "5", source: "7" },
-  { id: "9", target: "6", source: "7" },
-  { id: "10", target: "6", source: "8" },
-  { id: "11", target: "8", source: "8" },
-];
-
-onMounted(forceGraph({ nodes, links }, {}));
+import Tree from "../components/Draw/Tree.vue";
 </script>
 
 <template>
-  <div>
-    <svg id="canvas"></svg>
-  </div>
+  <Tree />
 </template>
 
-<style>
-.canvas {
-  width: 500px;
-  height: 500px;
-  border: 1px solid red;
-}
-
-.bar {
-  fill: #3f4f5f;
-}
-</style>
+<style></style>
